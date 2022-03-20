cc7207ab9f6ceeabf45528409b87a6adfcda894a
Iulian <iulianboerescu16@gmail.com>
2022-03-20 19:38:03 +0200

    My first homework!

---
+++ Tema1/main.py
@@ -0,0 +1,26 @@
+# Exercitiul 1
+first_list = [7, 8, 9, 2, 3, 1, 4, 10, 5, 6]
+print("Lista initiala este:", first_list)
+
+# Exercitiul 2
+second_list = sorted(first_list)
+print("Lista ordonata ascendent este: ", second_list)
+
+# Exercitiul 3
+third_list = sorted(first_list, reverse=True)
+print("Lista ordonata descendent este:", third_list)
+
+# Exercitiul 4
+fourth_list = second_list[1::2]
+print("Lista ce continere numerele pare este:", fourth_list)
+
+# Exercitiul 5
+fifth_list = second_list[0::2]
+print("Lista ce contine numerele impare este:", fifth_list)
+
+# Exercitiul 6
+sixth_list = second_list[2::3]
+print("Lista ce contine numerele multiple de 3 este:", sixth_list)
+
+print("Lista initiala este:", first_list)
+
